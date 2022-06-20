a215f25ded18ac8809b20de10b1751ca9e609d16
admin <admin@DESKTOP-UFRCN2K>
2022-06-20 08:12:47 -0700

    Commit and push the code to GIT

---
+++ SampleProject/src/com/github/koti/Student.java
@@ -0,0 +1,11 @@
+package com.github.koti;
+
+public class Student {
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("I am integrating jenkins with GitHub Reposistory");
+	}
+
+}
